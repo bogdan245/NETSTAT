6f2837095969104b82c083ec2ddc160cc3972a97
bogdan245 <pmbogdan96@gmail.com>
2022-09-20 15:03:44 +0300

    Finished main script - without specific lcoations

--- NetMap.py
+++ NetMap.py
@@ -1,11 +1,11 @@
 #Imports
 
-from icmplib import multiping, ping
+from icmplib import multiping
 import configparser
 import json
-import sys
 
-#Reading configuration files
+
+# Reading configuration files
 
 config = configparser.ConfigParser()
 config.read('network.ini')
@@ -13,3 +13,39 @@ sections = config.sections()
 
 sourceOptions = config.options('SOURCE')
 sourceItems = config.items('SOURCE')
+
+# Make lists for sources and targets
+src_list = []
+target_list = []
+
+for i in range(len(sourceItems)):
+    src_list.append(sourceItems[i][1])
+
+targetOptions = config.options('TARGET')
+targetItems = config.items('TARGET')
+
+for i in range(len(targetItems)):
+    target_list.append(targetItems[i][1])
+
+targetsDict = {}  # Empty dictionary to store targets
+
+# Not the most elegant solution, but it works
+
+# the next bit of code just pings multiple targets and outputs TRUE or FALSE in a json
+while True:
+    for s in src_list:
+        host = multiping(target_list, count=3, source=s) # pings every target 3 times
+        for h in range(len(host)):
+            if s == src_list[0]:
+                if host[h].is_alive:
+                    targetsDict[target_list[h]] = "TRUE"
+                else:
+                    targetsDict[target_list[h]] = "FALSE"
+        with open("targets.json", 'w') as output:
+            output.write(json.dumps(targetsDict))
+
+        # dump the json in a javascript file to import it in the webpage, another non-elegant solution, but it works
+        with open("declare.js", 'w') as json_formatted:
+            jsonobj = json.dumps(targetsDict)
+            json_formatted.write("var jsonstr = '{}'".format(jsonobj))
+            json_formatted.close()
