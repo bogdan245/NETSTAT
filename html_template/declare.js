777f1b0b838ce99f736bea8d16dd41579b783aee
bogdan245 <pmbogdan96@gmail.com>
2022-09-22 11:23:30 +0300

    Made some changes to display a demo result

--- html_template/declare.js
+++ html_template/declare.js
@@ -1,1 +1,1 @@
-var jsonstr = '{"192.168.5.132": "TRUE", "8.8.8.8": "FALSE", "5.5.5.5": "FALSE", "google.com": "FALSE"}'
+var jsonstr = '{"192.168.5.132": "TRUE", "8.8.8.8": "FALSE", "5.5.5.5": "TRUE", "google.com": "FALSE"}'
